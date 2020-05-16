e019d021a9609b571dd63a5c44f9994e65f8592c
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-05-15 22:26:03 -0500

    learning about calling callbacks

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+//* Step 1
+
+// function getIncome(callback) {
+//   // (#2) The callback is then called with 650 as it's argument
+//   callback(650);
+// }
+
+// // (#1) call getIncome function with a callback as an argument
+// getIncome(function (income) {
+//   // (#3) income is set to 650
+//   console.log(income); //650
+// });
+
+//=======================
+//* Step 2
+
+function getIncome(callback) {
+  callback(650);
+}
+
+function payRent(income, callback) {
+  callback(income - 200);
+}
+
+function payUtilityBills(income, callback) {
+  callback(income - 87);
+}
+
+function payInternetBill(income, callback) {
+  callback(income - 50);
+}
+
+getIncome(function (income) {
+  // call payRent inside "getIncome" callback
+  payRent(income, function (incomeAfterRent) {
+    console.log("Income after Rent: ", incomeAfterRent); //450
+  });
+  //! Below not correct, must change income (after bills) before calling next function
+  payUtilityBills(incomeAfterRent, function (incomeAfterUtility) {
+    console.log("Income after Utilities", incomeAfterUtility); //!563 (should be 363)
+  });
+  payInternetBill(incomeAfterInternet, function (incomeAfterInternet) {
+    console.log("Income after internet", incomeAfterInternet); //!600 (should be 313)
+  });
+});
+
 //===============================================
 //! First trial run working (without callbacks)
 
